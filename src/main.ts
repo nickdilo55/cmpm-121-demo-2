a47bb09720ebc4666a619137db5c1d509029268d
nikolai.dil <nick.dilo55@gmail.com>
2024-10-23 13:08:21 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,12 @@ const APP_NAME = "Noted";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.appendChild(title);
+
+const canvas = document.createElement("canvas");
+canvas.height = 256;
+canvas.width = 256;
+app.appendChild(canvas);

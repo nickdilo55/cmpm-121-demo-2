a5ac42b34ee1d6254a04f68513d79d201ba47fe6
nikolai.dil <nick.dilo55@gmail.com>
2024-10-27 21:08:42 -0700

    Step 6 Complete

--- src/main.ts
+++ src/main.ts
@@ -16,26 +16,31 @@ app.appendChild(canvas);
 
 const draw = canvas.getContext("2d")!;
 draw.strokeStyle = "white";
-draw.lineWidth = 1;
 
+let size = 1.5;
 let currDrawing = false;
 
 class markerLine {
     coordinates: number[][];
+    thickness: number;
 
-    constructor(x: number, y: number) {
+    constructor(x: number, y: number, thickness: number) {
         this.coordinates = [[x, y]];
+        this.thickness = thickness;
     }
 
     drag(x: number, y: number) {
         this.coordinates.push([x, y]);
     }
 
-    display(ctx: { beginPath: () => void; moveTo: (arg0: number, arg1: number) => void; lineTo: (arg0: number, arg1: number) => void; stroke: () => void; }) {
+    display(ctx: {
+      lineWidth: number; beginPath: () => void; moveTo: (arg0: number, arg1: number) => void; lineTo: (arg0: number, arg1: number) => void; stroke: () => void; 
+}) {
         if (this.coordinates.length < 2) {
             return;
         }
         ctx.beginPath();
+        ctx.lineWidth = this.thickness;
         ctx.moveTo(this.coordinates[0][0], this.coordinates[0][1]);
 
         for (let i = 1; i < this.coordinates.length; i++) {
@@ -47,12 +52,12 @@ class markerLine {
 }
 
 let redo: markerLine[];
-let curr: markerLine | null = null;
+let curr: markerLine | undefined;
 const drawingArr: markerLine[] = [];
 
 canvas.addEventListener("mousedown", (pos) => {
     currDrawing = true;
-    curr = new markerLine(pos.offsetX, pos.offsetY);
+    curr = new markerLine(pos.offsetX, pos.offsetY, size);
     curr.display(draw);
 });
 
@@ -67,7 +72,7 @@ canvas.addEventListener("mouseup", () => {
     if (curr && currDrawing) {
         drawingArr.push(curr); 
         currDrawing = false;
-        curr = null;
+        curr = undefined;
         drawingChanged();
         redo = [];
     }
@@ -93,7 +98,14 @@ const addButton = (text: string, clicked: () => void) => {
     button.textContent = text;
     button.addEventListener("click", clicked);
     app.appendChild(button);
+    return button;
 };
+
+const thick = addButton("THICK", () => size = 2.5);
+thick.classList.add("tool");
+
+const thin = addButton("THIN", () => size = 0.75);
+thin.classList.add("tool");
 
 addButton("UNDO", () => {
     if (drawingArr.length > 0) {
